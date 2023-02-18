c84f9108f49d936825a9864128efd51432cfeff7
Claudio Manoel <sh.claudio@gmail.com>
2023-02-17 21:58:52 -0300

    Versão inicial

---
+++ kelvin.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var ebulicaoK = 373.0
+	var ebulicaoC = ebulicaoK - 273.0
+
+	fmt.Printf("A temperatura de ebulição da água em °K é de %g° e em °C é de %g°", ebulicaoK, ebulicaoC)
+}
